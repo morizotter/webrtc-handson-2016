9cb560320b89f516d71b9c5a24e5e0329e8a2e4c
Naoki Morita <namorit@gmail.com>
2017-04-09 14:09:44 +0900

    Write startVideo, playVideo

--- client/webrtc.js
+++ client/webrtc.js
@@ -4,3 +4,20 @@ const textForSendSdp = document.getElementById('text_for_send_sdp');
 const textToReceiveSdp = document.getElementById('text_for_receive_sdp');
 let localStream = null;
 let peerConnection = null;
+
+function startVideo() {
+    navigator.mediaDevices.getUserMedia({video: true, audio: true})
+    .then(function (stream) { // success
+        playVideo(localVideo, stream);
+        localStream = stream;
+    }).catch(function (error) { // error
+        console.error("mediaDevice.getUserMedia() error: ", error);
+        return;
+    });
+}
+
+function playVideo(elemenet, stream) {
+    element.srcObject = stream;
+    element.play();
+}
+
