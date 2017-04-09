d159120e3ad2bbdb7822e6649f0389c75cc699a1
Naoki Morita <namorit@gmail.com>
2017-04-09 15:58:01 +0900

    Update

--- client/webrtc.js
+++ client/webrtc.js
@@ -1,59 +1,3 @@
-// シグナリングサーバへ接続する
-const wsUrl = "ws://localhost:3001";
-const ws = new WebSocket(wsUrl);
-ws.onopen = function (evt) {
-    console.log("ws open()");
-};
-ws.onerror = function (err) {
-    console.error("ws onerror() ERR:", err);
-};
-ws.onmessage = function (evt) {
-    console.log("ws onerror() ERR:", err);
-    const message = JSON.parse(evt.data);
-    if (message.type === "offer") {
-        // offer 受信時
-        console.log("Received offer ...");
-        textToReceiveSdp.value = message.sdp;
-        const offer = new RTCSessionDescription(message);
-        setOffer(offer);
-    }
-    else if (message.type === "answer") {
-        // answer受信時
-        console.log("Received answer ...");
-        textToReceiveSdp.value = message.sdp;
-        const answer = new RTCSessionDescription(message);
-        setAnswer(answer);
-    }
-    else if (message.type === "candidate") {
-        // ICE candidate 受信時
-        console.log("Received ICE candidate ...");
-        const candidate = new RTCIceCandidate(message.ice);
-        console.log(candidate);
-        addIceCandidate(candidate);
-    }
-};
-
-// ICE candidate受信時にセットする
-function addIceCandidate(candidate) {
-    if (peerConnection) {
-        peerConnection.addIceCandidate(candidate);
-    }
-    else {
-        console.error("PeerConnection not exist!");
-        return;
-    }
-}
-
-// ICE candidate生成時に送信する
-function sendIceCandidate(candidate) {
-    console.log("--- sending ICE candidate ---");
-    const message = JSON.stringify({ type: "candidate", ice: candidate });
-    console.log("sending candidate=" + message);
-    ws.send(message);
-}
-
-
-
 const localVideo = document.getElementById('local_video');
 const remoteVideo = document.getElementById('remote_video');
 const textForSendSdp = document.getElementById('text_for_send_sdp');
@@ -61,138 +5,163 @@ const textToReceiveSdp = document.getElementById('text_for_receive_sdp');
 let localStream = null;
 let peerConnection = null;
 
+// シグナリングサーバへ接続する
+const wsUrl = 'ws://localhost:3001/';
+const ws = new WebSocket(wsUrl);
+ws.onopen = function (evt) {
+    console.log('ws open()');
+};
+ws.onerror = function (err) {
+    console.error('ws onerror() ERR:', err);
+};
+ws.onmessage = function (evt) {
+    console.log('ws onmessage() data:', evt.data);
+    const message = JSON.parse(evt.data);
+    if (message.type === 'offer') {
+        // offer 受信時
+        console.log('Received offer ...');
+        textToReceiveSdp.value = message.sdp;
+        const offer = new RTCSessionDescription(message);
+        setOffer(offer);
+    }
+    else if (message.type === 'answer') {
+        // answer 受信時
+        console.log('Received answer ...');
+        textToReceiveSdp.value = message.sdp;
+        const answer = new RTCSessionDescription(message);
+        setAnswer(answer);
+    }
+    else if (message.type === 'candidate') {
+        // ICE candidate 受信時
+        console.log('Received ICE candidate ...');
+        const candidate = new RTCIceCandidate(message.ice);
+        console.log(candidate);
+        addIceCandidate(candidate);
+    }
+    else if (message.type === 'close') {
+        // closeメッセージ受信時
+        console.log('peer is closed ...');
+        hangUp();
+    }
+};
+
+// ICE candaidate受信時にセットする
+function addIceCandidate(candidate) {
+    if (peerConnection) {
+        peerConnection.addIceCandidate(candidate);
+    }
+    else {
+        console.error('PeerConnection not exist!');
+        return;
+    }
+}
+
+// ICE candidate生成時に送信する
+function sendIceCandidate(candidate) {
+    console.log('---sending ICE candidate ---');
+    const message = JSON.stringify({ type: 'candidate', ice: candidate });
+    console.log('sending candidate=' + message);
+    ws.send(message);
+}
+
+// getUserMediaでカメラ、マイクにアクセス
 function startVideo() {
     navigator.mediaDevices.getUserMedia({ video: true, audio: true })
         .then(function (stream) { // success
             playVideo(localVideo, stream);
             localStream = stream;
         }).catch(function (error) { // error
-            console.error("mediaDevice.getUserMedia() error: ", error);
+            console.error('mediaDevice.getUserMedia() error:', error);
             return;
         });
 }
 
+// Videoの再生を開始する
 function playVideo(element, stream) {
     element.srcObject = stream;
     element.play();
 }
 
-function stopVideo(element, stream) {
-    console.log("stopVideo: not implemented");
-}
-
 // WebRTCを利用する準備をする
 function prepareNewConnection() {
     // RTCPeerConnectionを初期化する
-    // NAT越えを手助けするユーティリティであるStunサーバ、TURNサーバのURLや認証情報を指定する
-    // ローカル環境で試験する場合はSTUN/TURN共に設定せず共繋がるが、練習のためにSkyWayが提供するSTUNサーバを設定してみる
-    const pc_config = { "iceServers": [{ urls: "stun:stun.skyway.io:3478" }] };
+    const pc_config = { "iceServers": [{ "urls": "stun:stun.skyway.io:3478" }] };
     const peer = new RTCPeerConnection(pc_config);
 
     // リモートのストリームを受信した場合のイベントをセット
-    if ("ontrack" in peer) {
-        // Firefox向け
+    if ('ontrack' in peer) {
         peer.ontrack = function (event) {
-            console.log("-- peer.ontrack()");
+            console.log('-- peer.ontrack()');
             playVideo(remoteVideo, event.streams[0]);
         };
     }
     else {
-        // Chrome向け
         peer.onaddstream = function (event) {
-            console.log("-- peer.onaddstream()");
+            console.log('-- peer.onaddstream()');
             playVideo(remoteVideo, event.stream);
-        }
+        };
     }
 
-    // ICE Candidateを収集した時のイベント
-
-    // Valilla ICE
+    // ICE Candidateを収集したときのイベント
     peer.onicecandidate = function (evt) {
         if (evt.candidate) {
             console.log(evt.candidate);
             sendIceCandidate(evt.candidate);
         } else {
-            console.log("empty ice event");
+            console.log('empty ice event');
+            // sendSdp(peer.localDescription);
         }
     };
 
-    // // Tricle ICE
-    // peer.onicecandidate = function (evt) {
-    //     if (evt.candidate) {
-    //         // Trickle ICE の場合は、ICE candidateを相手に送る               
-    //         console.log(evt.candidate);
-    //         sendIceCandidate(evt.candidate);
-    //     } else {
-    //         // Trickle ICEの場合は何もしない
-    //         console.log('empty ice event');
-    //     }
-    // };
-
-    // ローカルのストリームを利用できるように準備する
-    if (localStream) {
-        console.log("Adding local stream...");
-        peer.addStream(localStream);
-    }
-    else {
-        console.warn("no local stream, but continue.");
-    }
-
-    // ICEのステータスを変更になった時の処理
+    // ICEのステータスが変更になったときの処理
     peer.oniceconnectionstatechange = function () {
-        console.log("ICE connections Status has changed to " + peer.iceConnectionState);
+        console.log('ICE connection Status has changed to ' + peer.iceConnectionState);
         switch (peer.iceConnectionState) {
-            case "closed":
-            case "failed":
-                // ICEのステートが切断状態または以上状態になったら切断処理を実行する
+            case 'closed':
+            case 'failed':
+                // ICEのステートが切断状態または異常状態になったら切断処理を実行する
                 if (peerConnection) {
                     hangUp();
                 }
                 break;
-            case "dissconnected":
+            case 'dissconnected':
                 break;
-        };
+        }
+    };
+
+    // ローカルのストリームを利用できるように準備する
+    if (localStream) {
+        console.log('Adding local stream...');
+        peer.addStream(localStream);
+    }
+    else {
+        console.warn('no local stream, but continue.');
     }
 
     return peer;
 }
 
-// P2P通信を切断する
-function hangUp() {
-    if (peerConnection) {
-        if (peerConnection.iceConnectionState !== "closed") {
-            peerConnection.close();
-            peerConnection = null;
-            cleanupVideoElement(remoteVideo);
-            textForSendSdp.value = "";
-            return;
-        }
-    }
-    console.log("peerConnection is closed.");
-}
-
-// ビデオエレメントを初期化する
-function cleanupVideoElement(element) {
-    element.pause();
-    element.srcObject = null;
-}
-
+// 手動シグナリングのための処理を追加する
 function sendSdp(sessionDescription) {
-    console.log("---sending sdp ---");
+    console.log('---sending sdp ---');
     textForSendSdp.value = sessionDescription.sdp;
+    /*---
+     textForSendSdp.focus();
+     textForSendSdp.select();
+     ----*/
     const message = JSON.stringify(sessionDescription);
-    console.log("sending SDP=" + message);
+    console.log('sending SDP=' + message);
     ws.send(message);
 }
 
 // Connectボタンが押されたら処理を開始
 function connect() {
     if (!peerConnection) {
-        console.log("make Offer");
+        console.log('make Offer');
         makeOffer();
     }
     else {
-        console.warn("peer already exist.");
+        console.warn('peer already exist.');
     }
 }
 
@@ -215,8 +184,8 @@ function makeOffer() {
 
 // Answer SDPを生成する
 function makeAnswer() {
-    console.log('sending Answer. Creating remote session description...' );
-    if (! peerConnection) {
+    console.log('sending Answer. Creating remote session description...');
+    if (!peerConnection) {
         console.error('peerConnection NOT exist!');
         return;
     }
@@ -224,19 +193,19 @@ function makeAnswer() {
         .then(function (sessionDescription) {
             console.log('createAnswer() succsess in promise');
             return peerConnection.setLocalDescription(sessionDescription);
-        }).then(function() {
+        }).then(function () {
             console.log('setLocalDescription() succsess in promise');
             sendSdp(peerConnection.localDescription);
-        }).catch(function(err) {
+        }).catch(function (err) {
             console.error(err);
-    });
+        });
 }
 
 // SDPのタイプを判別しセットする
 function onSdpText() {
     const text = textToReceiveSdp.value;
     if (peerConnection) {
-        // Offerした側が相手からのAnswerをセットする場合
+        // Offerした側が相手からのAnserをセットする場合
         console.log('Received answer text...');
         const answer = new RTCSessionDescription({
             type: 'answer',
@@ -285,4 +254,28 @@ function setAnswer(sessionDescription) {
         }).catch(function (err) {
             console.error('setRemoteDescription(answer) ERROR: ', err);
         });
+}
+
+// P2P通信を切断する
+function hangUp() {
+    if (peerConnection) {
+        if (peerConnection.iceConnectionState !== 'closed') {
+            peerConnection.close();
+            peerConnection = null;
+            const message = JSON.stringify({ type: 'close' });
+            console.log('sending close message');
+            ws.send(message);
+            cleanupVideoElement(remoteVideo);
+            textForSendSdp.value = '';
+            textToReceiveSdp.value = '';
+            return;
+        }
+    }
+    console.log('peerConnection is closed.');
+}
+
+// ビデオエレメントを初期化する
+function cleanupVideoElement(element) {
+    element.pause();
+    element.srcObject = null;
 }
